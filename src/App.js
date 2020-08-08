0f5c4bf1a0dda71b897bea798c1e2ac0ba75d607
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-08-08 22:24:02 +0300

    feat: implement audio player

--- src/App.js
+++ src/App.js
@@ -10,112 +10,105 @@ import EmptyDescriptionBlock from "./Common/DescriptionBlock/EmptyDescriptionBlo
 class App extends React.Component {
 
     state = {
-        score:0,
+        score: 0,
         numberOfLevel: 0,
         randomQuestionNumber: 0,
-        quantityAnswer:0,
+        quantityAnswer: 0,
         userAnswer: null,
         answerStatus: false,
-           };
+    };
 
- componentDidMount() {
-     this.start()
- }
+    componentDidMount() {
+        this.start()
+    }
 
-    start= ()=> {
+    start = () => {
         let randomNumber = Math.floor(Math.random() * birdsData[this.state.numberOfLevel].length);
         this.setState({randomQuestionNumber: randomNumber});
-                     };
+    };
 
 
-
-    nextLevel= ()=> {
-        if (this.state.numberOfLevel >= birdsData[this.state.numberOfLevel].length-1  ) {
-            alert ("you is winner, game is over");
+    nextLevel = () => {
+        if (this.state.numberOfLevel >= birdsData[this.state.numberOfLevel].length - 1) {
+            alert("you is winner, game is over");
             this.setState({answerStatus: true});
-        }
-        else {
+        } else {
             this.setState({numberOfLevel: this.state.numberOfLevel + 1});
             this.setState({quantityAnswer: 0});
             this.setState({userAnswer: null});
             this.setState({answerStatus: false});
             this.start()
         }
-                   };
-
-
-    makeAnswer=(e)=> {
-            this.setState({userAnswer: e}, this.checkAnswer)
     };
 
-    checkAnswer=()=> {
+
+    makeAnswer = (e) => {
+        this.setState({userAnswer: e}, this.checkAnswer)
+    };
+
+    checkAnswer = () => {
 
         let bonus = 5 - this.state.quantityAnswer;
 
         if (this.state.randomQuestionNumber == this.state.userAnswer) {
 
             this.setState({answerStatus: true});
-            this.setState({score: this.state.score+bonus});
+            this.setState({score: this.state.score + bonus});
 
             // then -->  ring bell + next level is active !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            alert("win " + this.state.randomQuestionNumber + this.state.userAnswer)}
+            alert("win " + this.state.randomQuestionNumber + this.state.userAnswer)
+        } else {
+            alert("NOT " + this.state.randomQuestionNumber + this.state.userAnswer)
+        }
 
-        else { alert("NOT " + this.state.randomQuestionNumber + this.state.userAnswer)}
-
-        this.setState({quantityAnswer: this.state.quantityAnswer+1});
+        this.setState({quantityAnswer: this.state.quantityAnswer + 1});
 
     }
 
 
+    render = () => {
 
-
-
-render = () => {
-
-            let ArrayBird= birdsData[this.state.numberOfLevel];
+        let ArrayBird = birdsData[this.state.numberOfLevel];
 
         return (
+
+
             <div className="App">
-                <header className="App-header">
 
-                     <Header
-                        numberOfLevel={this.state.numberOfLevel}
-                        score={this.state.score}
-                     />
+                <div>
+                <Header
+                    numberOfLevel={this.state.numberOfLevel}
+                    score={this.state.score}                 />
+                </div>
+
+                     <div>
+                <QuestionBlock
+                    answerStatus={this.state.answerStatus}
+                    nameBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].name}
+                    imgBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].image}
+                    audioBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].audio}
+                />
+                     </div>
 
 
-                    <QuestionBlock
-                        answerStatus={this.state.answerStatus}
-                        nameBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].name}
-                        imgBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].image}
-                        audioBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].audio}
+                <div className="answerAndDescr">
+                    <AnswerBlock
+                        ArrayBird={ArrayBird}
+                        makeAnswer={this.makeAnswer}
+                        userAnswer={this.state.userAnswer}
                     />
 
+                    {(this.state.userAnswer) ?
+                        <DescriptionBlock
+                            arrayBird={birdsData[this.state.numberOfLevel][this.state.userAnswer]}
+                        />
+                        :
+                        <EmptyDescriptionBlock/>}
+                </div>
+
+                <button onClick={this.nextLevel}>next level</button>
 
 
-                    <div className="answerAndDescr">
-                            <AnswerBlock
-                                 ArrayBird={ArrayBird}
-                                 makeAnswer={this.makeAnswer}
-                                 userAnswer={this.state.userAnswer}
-                            />
-
-                            { (this.state.userAnswer) ?
-                            <DescriptionBlock
-                                arrayBird={birdsData[this.state.numberOfLevel][this.state.userAnswer]}
-                                />
-                                :
-                                <EmptyDescriptionBlock/>}
-
-                    </div>
-
-
-                    <button className="--------------next level--------------"
-                        onClick={this.nextLevel} >next level</button>
-
-
-
-                </header>
             </div>
         );
     }
