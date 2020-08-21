4bbfe19498b0ebb8065c46d5b6595e7e2faa3ecf
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-08-21 23:31:14 +0300

    feat: add sound for button

--- src/App.js
+++ src/App.js
@@ -2,131 +2,68 @@ import React from 'react';
 import birdsData from "./Common/birdsData/birdsData";
 import './App.css';
 import Header from "./Common/Header/Header";
-import QuestionBlock from "./Common/Question/QuestionBlock";
+import Question from "./Common/Question/Question";
 import AnswerBlock from "./Common/AnswerBlock/AnswerBlock";
 import DescriptionBlock from "./Common/DescriptionBlock/DescriptionBlock";
-import EmptyDescriptionBlock from "./Common/DescriptionBlock/EmptyDescriptionBlock";
+
 
 class App extends React.Component {
 
+
     state = {
-        score: 0,
-        numberOfLevel: 0,
-        randomQuestionNumber: 0,
-        quantityAnswer: 0,
-        userAnswer: null,
-        answerStatus: false,
-        birdData:[]
+        filterHeaderQuestion: {
+            filterCss1: "passive",
+            filterCss2: "passive",
+            filterCss3: "passive",
+            filterCss4: "passive",
+            filterCss5: "active",
+        },
+       numberOfQuestion: 0,
     };
 
-    options = [
-        {style: "style1", checked: false},
-        {style: "style2", checked: false},
-        {style: "style3", checked: false},
-        {style: "style4", checked: false},
-        {style: "style5", checked: false},
-        {style: "style6", checked: false}   ];
 
 
-    componentDidMount() {
-        this.start();
-        let tempBirdData = birdsData[this.state.numberOfLevel].map((element, index) => {
-            return {...element, ...this.options[index]}
-        });
-        this.setState({birdData: tempBirdData});
-        alert ("didmountwork")
-       }
-
-    start = () => {
-        let randomNumber = Math.floor(Math.random() * birdsData[this.state.numberOfLevel].length);
-        this.setState({randomQuestionNumber: randomNumber});
+    test1= ()=> {
+        alert("next question");
+        this.setState({
+            filterHeaderQuestion: {...this.state.filterHeaderQuestion, filterCss2: "active"} } );
+             // console.log (block)
     };
 
-    nextLevel = () => {
-        if (this.state.numberOfLevel >= birdsData[this.state.numberOfLevel].length - 1) {
-            alert("you is winner, game is over");
-            this.setState({answerStatus: true});
-        } else {
-            this.setState({numberOfLevel: this.state.numberOfLevel + 1});
-            this.setState({quantityAnswer: 0});
-            this.setState({userAnswer: null});
-            this.setState({answerStatus: false});
-            this.start();
-                let resetBirdDataChecked=this.state.birdData.map ((element) => {
-                    { return {...element, checked: false }
-                    } } );
-                this.setState({birdData: resetBirdDataChecked});
-        } };
-
-    makeAnswer = (id) => {
-        this.setState({userAnswer:id}, this.checkAnswer);
-        let birdDataNew=this.state.birdData.map ((element, index) => {
-             if (index == id) {{
-                    return {...element, checked: true }
-                }} else { return element }} );
-        this.setState({birdData: birdDataNew})  };
 
 
-    checkAnswer = () => {
-        let bonus = 5 - this.state.quantityAnswer;
-        if (this.state.randomQuestionNumber == this.state.userAnswer) {
-            this.setState({answerStatus: true});
-            this.setState({score: this.state.score + bonus});
-            // then -->  ring bell + next level is active !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            alert("win " + this.state.randomQuestionNumber + this.state.userAnswer)
-        } else {
-            alert("NOT " + this.state.randomQuestionNumber + this.state.userAnswer)
-        }
-        this.setState({quantityAnswer: this.state.quantityAnswer + 1});
-    }
 
 
     render = () => {
 
-        // let ArrayBird = birdsData[this.state.numberOfLevel];
+        let ArrayBird= birdsData[this.state.numberOfQuestion].map (element =>
+                element.name  );
 
         return (
-                <div className="App">
-                <div className={"mainWrapper"}>
-                <div>
-                        <Header
-                            numberOfLevel={this.state.numberOfLevel}
-                            score={this.state.score}                 />
-                </div>
-
-                <div>
-                        <QuestionBlock
-                            answerStatus={this.state.answerStatus}
-                            nameBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].name}
-                            imgBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].image}
-                            audioBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].audio}
-                        />
-                 </div>
+            <div className="App">
+                <header className="App-header">
 
 
-                <div className="answerAndDescr">
-                    <AnswerBlock
-                        ArrayBird={this.state.birdData}
-                        makeAnswer={this.makeAnswer}
-                        userAnswer={this.state.userAnswer}
-                        answerStatus={this.state.answerStatus}
-                    />
+                    <Header
+                        filterHeaderQuestion={this.state.filterHeaderQuestion}
+                        changeFilter={this.test1}/>
 
-                    {(this.state.userAnswer) ?
-                        <DescriptionBlock
-                            arrayBird={birdsData[this.state.numberOfLevel][this.state.userAnswer]}
-                            audioBird={birdsData[this.state.numberOfLevel][this.state.userAnswer].audio}
-                        />
-                        :
-                        <EmptyDescriptionBlock/>}
-                </div>
+                    <Question/>
 
-                <button className={!this.state.answerStatus ? "nextLevelButtonDisabled" : "nextLevelButtonEnabled" } onClick={this.nextLevel} disabled={!this.state.answerStatus} >next level</button>
 
-                </div>
+                    {/*{QuestionArray}*/}
+
+
+                    <div className="ansAndDescr">
+                        <AnswerBlock ArrayBird={ArrayBird} />
+                        <DescriptionBlock/>
+                    </div>
+
+                    <button onClick={this.test1}>gggg</button>
+
+
+                </header>
             </div>
-
-
         );
     }
 }
