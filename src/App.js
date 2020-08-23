0c29b97a61092f6d1d8e51e941d459f6aa4ee8f9
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-08-23 12:43:14 +0300

    fix: relayout header

--- src/App.js
+++ src/App.js
@@ -1,69 +1,148 @@
 import React from 'react';
 import birdsData from "./Common/birdsData/birdsData";
 import './App.css';
+import soundPositive from './Common/AnswerBlock/sound/pos.mp3'
+import soundNegative from './Common/AnswerBlock/sound/neg.mp3'
 import Header from "./Common/Header/Header";
-import Question from "./Common/Question/Question";
+import QuestionBlock from "./Common/Question/QuestionBlock";
 import AnswerBlock from "./Common/AnswerBlock/AnswerBlock";
 import DescriptionBlock from "./Common/DescriptionBlock/DescriptionBlock";
-
+import EmptyDescriptionBlock from "./Common/DescriptionBlock/EmptyDescriptionBlock";
 
 class App extends React.Component {
 
-
     state = {
-        filterHeaderQuestion: {
-            filterCss1: "passive",
-            filterCss2: "passive",
-            filterCss3: "passive",
-            filterCss4: "passive",
-            filterCss5: "active",
-        },
-       numberOfQuestion: 0,
+        score: 0,
+        numberOfLevel: 0,
+        randomQuestionNumber: 0,
+        quantityAnswer: 0,
+        userAnswer: null,
+        answerStatus: false,
+        gameOver: false,
+        birdData:[]
     };
 
+    options = [
+        {style: "style1", checked: false},
+        {style: "style2", checked: false},
+        {style: "style3", checked: false},
+        {style: "style4", checked: false},
+        {style: "style5", checked: false},
+        {style: "style6", checked: false}   ];
 
 
-    test1= ()=> {
-        alert("next question");
-        this.setState({
-            filterHeaderQuestion: {...this.state.filterHeaderQuestion, filterCss2: "active"} } );
-             // console.log (block)
+    componentDidMount() {
+        this.start();
+
+         }
+
+    start = () => {
+        let randomNumber = Math.floor(Math.random() * birdsData[this.state.numberOfLevel].length);
+        this.setState({randomQuestionNumber: randomNumber});
+
+        let tempBirdData = birdsData[this.state.numberOfLevel].map((element, index) => {
+            return {...element, ...this.options[index]}
+        });
+        this.setState({birdData: tempBirdData});
+          };
+
+    nextLevel = () => {
+        if (this.state.numberOfLevel >= birdsData.length - 1) {
+            alert("Игра окончена");
+            this.setState({answerStatus: true});
+        } else {
+            this.setState({numberOfLevel: this.state.numberOfLevel + 1}, this.start);
+            this.setState({quantityAnswer: 0});
+            this.setState({userAnswer: null});
+            this.setState({answerStatus: false});
+                          let resetBirdDataChecked=this.state.birdData.map ((element) => {
+                    { return {...element, checked: false }
+                    } } );
+                this.setState({birdData: resetBirdDataChecked});
+
+        } };
+
+    makeAnswer = (id) => {
+        this.setState({userAnswer:id}, this.checkAnswer);
+        let birdDataNew=this.state.birdData.map ((element, index) => {
+             if (index == id) {{
+                    return {...element, checked: true }
+                }} else { return element }} );
+        this.setState({birdData: birdDataNew})  };
+
+
+    checkAnswer = () => {
+        let bonus = 5 - this.state.quantityAnswer;
+        if (this.state.randomQuestionNumber == this.state.userAnswer) {
+            this.setState({answerStatus: true});
+            this.setState({score: this.state.score + bonus});
+            this.player1.play();
+        } else {
+            this.player2.play();
+        }
+        this.setState({quantityAnswer: this.state.quantityAnswer + 1});
     };
-
-
-
 
 
     render = () => {
 
-        let ArrayBird= birdsData[this.state.numberOfQuestion].map (element =>
-                element.name  );
-
         return (
-            <div className="App">
-                <header className="App-header">
+                <div className="App">
+                <div className={"mainWrapper"}>
+                <div>
+
+                    <audio src={soundPositive} ref={ref => (this.player1 = ref)} ></audio>
+                    <audio src={soundNegative} ref={ref => (this.player2 = ref)} ></audio>
+
+                        <Header
+                            numberOfLevel={this.state.numberOfLevel}
+                            score={this.state.score}                 />
+                </div>
+
+                <div>
+                        <QuestionBlock
+                            answerStatus={this.state.answerStatus}
+                            nameBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].name}
+                            imgBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].image}
+                            audioBird={birdsData[this.state.numberOfLevel][this.state.randomQuestionNumber].audio}
+                        />
+                 </div>
 
 
-                    <Header
-                        filterHeaderQuestion={this.state.filterHeaderQuestion}
-                        changeFilter={this.test1}/>
+                <div className="answerAndDescr">
+                    <AnswerBlock
+                        ArrayBird={this.state.birdData}
+                        makeAnswer={this.makeAnswer}
+                        userAnswer={this.state.userAnswer}
+                        answerStatus={this.state.answerStatus}
+                        randomQuestionNumber = {this.state.randomQuestionNumber}
+                      />
 
-                    <Question/>
+                    {(this.state.userAnswer) ?
+                        <DescriptionBlock
+                            arrayBird={birdsData[this.state.numberOfLevel][this.state.userAnswer]}
+                            audioBird={birdsData[this.state.numberOfLevel][this.state.userAnswer].audio}
+                        />
+                        :
+                        <EmptyDescriptionBlock/>}
+                </div>
 
 
-                    {/*{QuestionArray}*/}
-
-
-                    <div className="ansAndDescr">
-                        <AnswerBlock ArrayBird={ArrayBird} />
-                        <DescriptionBlock/>
-                    </div>
-
-                    <button onClick={this.test1}>gggg</button>
-
-
-                </header>
+                    {  (this.state.answerStatus==true
+                        && this.state.numberOfLevel == 5) ?
+                                                           <button className={"nextLevelButtonEnabled"} >
+                                                           Поздравляем! Вы прошли викторину и набрали
+                                                           {this.state.score} из 30 возможных баллов </button>
+                                                           :
+                                                           <button className={!this.state.answerStatus ?
+                                                           "nextLevelButtonDisabled" : "nextLevelButtonEnabled" }
+                                                           onClick={this.nextLevel}
+                                                           disabled={!this.state.answerStatus}>
+                                                           next level</button>}
+                </div>
             </div>
+
+
         );
     }
 }
