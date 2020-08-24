5088d356d3d5963104d63b7e8488fd8f2f506eed
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-08-24 21:00:43 +0300

    fix: deleted comments

--- src/Common/AnswerBlock/AnswerBlock.jsx
+++ src/Common/AnswerBlock/AnswerBlock.jsx
@@ -5,8 +5,7 @@ class AnswerBlock extends React.Component {
 
     makeAnswer=(e)=> {
         this.props.makeAnswer(e.currentTarget.id)
-           // function (id)
-         };
+            };
 
 
 
@@ -22,8 +21,7 @@ class AnswerBlock extends React.Component {
                              checked={element.checked}
                              id={index}
                              onChange={this.makeAnswer}
-                             // onChange={()=>this.makeAnswer(element.id)}
-                         />
+                                />
                          <span className={ element.checked ?
                              (index==this.props.randomQuestionNumber ? styles.positive : styles.negative)
                              : styles.passive }>
