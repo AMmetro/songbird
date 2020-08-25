ebf610eadea9558d69d86a999fcf68439120111a
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-08-25 21:07:07 +0300

    add deploy

---
+++ pub/precache-manifest.9bca5584ea13d897073746ec2e005f61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b030a1a606ac53c00ac1f0b612d418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24807bf6d23da8ba1968",
+    "url": "/static/css/main.d0c6bc01.chunk.css"
+  },
+  {
+    "revision": "394568b2cffe7887af1c",
+    "url": "/static/js/2.e56f9830.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e56f9830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24807bf6d23da8ba1968",
+    "url": "/static/js/main.cf4d0067.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "be13e6aadd1240c2f000fed6c54b66b3",
+    "url": "/static/media/anonimous.be13e6aa.jpg"
+  },
+  {
+    "revision": "b115ede59a2b312f37a61f31b0fa70ae",
+    "url": "/static/media/logo.b115ede5.jpg"
+  },
+  {
+    "revision": "ffb37db0b80cd383503cb915c6896b1f",
+    "url": "/static/media/neg.ffb37db0.mp3"
+  },
+  {
+    "revision": "d0c5f2229fb79adc465123e0629687bb",
+    "url": "/static/media/pos.d0c5f222.mp3"
+  }
+]);
